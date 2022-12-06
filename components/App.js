f599a5a7946897305ed91d4dcaf97df49f932397
msdiniz <msdiniz@gmail.com>
2022-12-06 15:27:21 -0300

    modified to clip-07-refactor-to-custom-react-hook

--- components/App.js
+++ components/App.js
@@ -3,11 +3,21 @@
 //Global CSS cannot be imported from files other than your Custom <App>. Due to the Global nature of // stylesheets, and to avoid conflicts, Please move all first-party global CSS imports to pages/_app.js.
 import NoteList from "./NoteList.js";
 import Bootstrapfontawesometest from "./Bootstrapfontawesometest.js";
+import useNotes from "../hooks/useNotes";
 
 function App() {
+  const { notesData, notesDataError } = useNotes();
+
+  if (notesDataError) {
+    return <div className="container">error: {notesDataError}</div>;
+  }
+  if (!notesData) {
+    return <div className="container">...loading</div>;
+  }
+
   return (
     <div className="container">
-      <NoteList />
+      <NoteList notesData={notesData} />
       {/* <Bootstrapfontawesometest /> */}
     </div>
   );
--- components/NoteList.js
+++ components/NoteList.js
@@ -1,51 +1,51 @@
 import NoteCard from "./NoteCard.js";
-import notes from "../data/notes.json";
-import { useEffect, useState } from "react";
+// import notes from "../data/notes.json";
+// import { useEffect, useState } from "react";
 
-function NoteList() {
-  // const notesData = [
-  //   {
-  //     id: "1",
-  //     title: "Get Hotel",
-  //     description: "Prefer Best Westerns",
-  //     createDate: "2021-07-11T13:32:10.000Z",
-  //   },
-  //   {
-  //     id: "2",
-  //     title: "Fill Care With Gas",
-  //     description: "Make sure to get Shell",
-  //     createDate: "2021-07-11T13:31:34.000Z",
-  //   },
-  //   {
-  //     id: "3",
-  //     title: "Pack Bathing Suit",
-  //     description: "Make sure to bring extra bathing suite",
-  //     createDate: "2021-07-11T13:33:07.000Z",
-  //   },
-  // ];
-  //const notesData = notes;
-  //It allowed ^^^^ to move notes to another file, as a json, an not more a constant
-  const [notesData, setNotesData] = useState();
-  const [notesDataError, setNotesDataError] = useState();
+function NoteList({ notesData }) {
+  // // const notesData = [
+  // //   {
+  // //     id: "1",
+  // //     title: "Get Hotel",
+  // //     description: "Prefer Best Westerns",
+  // //     createDate: "2021-07-11T13:32:10.000Z",
+  // //   },
+  // //   {
+  // //     id: "2",
+  // //     title: "Fill Care With Gas",
+  // //     description: "Make sure to get Shell",
+  // //     createDate: "2021-07-11T13:31:34.000Z",
+  // //   },
+  // //   {
+  // //     id: "3",
+  // //     title: "Pack Bathing Suit",
+  // //     description: "Make sure to bring extra bathing suite",
+  // //     createDate: "2021-07-11T13:33:07.000Z",
+  // //   },
+  // // ];
+  // //const notesData = notes;
+  // //It allowed ^^^^ to move notes to another file, as a json, an not more a constant
+  // // const [notesData, setNotesData] = useState();
+  // // const [notesDataError, setNotesDataError] = useState();
 
-  useEffect(() => {
-    async function getData() {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      try {
-        setNotesData(notes);
-      } catch (e) {
-        setNotesDataError(e);
-      }
-    }
-    getData();
-  });
+  // // useEffect(() => {
+  // //   async function getData() {
+  // //     await new Promise((resolve) => setTimeout(resolve, 1000));
+  // //     try {
+  // //       setNotesData(notes);
+  // //     } catch (e) {
+  // //       setNotesDataError(e);
+  // //     }
+  // //   }
+  // //   getData();
+  // // });
 
-  if (notesDataError) {
-    return <div>error: {notesDataError}</div>;
-  }
-  if (!notesData) {
-    return <div>...loading</div>;
-  }
+  // if (notesDataError) {
+  //   return <div>error: {notesDataError}</div>;
+  // }
+  // if (!notesData) {
+  //   return <div>...loading</div>;
+  // }
   function sortByDate(a, b) {
     const dateA = a.createDate;
     const dateB = b.createDate;
