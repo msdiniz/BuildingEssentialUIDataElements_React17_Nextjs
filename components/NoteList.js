749da98eb6c2fabc599909623072fb61831ff461
msdiniz <msdiniz@gmail.com>
2022-12-06 15:11:37 -0300

    Implemented loading with setTimeOut and sorting cards

--- components/NoteList.js
+++ components/NoteList.js
@@ -1,5 +1,6 @@
 import NoteCard from "./NoteCard.js";
 import notes from "../data/notes.json";
+import { useEffect, useState } from "react";
 
 function NoteList() {
   // const notesData = [
@@ -22,10 +23,38 @@ function NoteList() {
   //     createDate: "2021-07-11T13:33:07.000Z",
   //   },
   // ];
-  const notesData = notes;
+  //const notesData = notes;
+  //It allowed ^^^^ to move notes to another file, as a json, an not more a constant
+  const [notesData, setNotesData] = useState();
+  const [notesDataError, setNotesDataError] = useState();
+
+  useEffect(() => {
+    async function getData() {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      try {
+        setNotesData(notes);
+      } catch (e) {
+        setNotesDataError(e);
+      }
+    }
+    getData();
+  });
+
+  if (notesDataError) {
+    return <div>error: {notesDataError}</div>;
+  }
+  if (!notesData) {
+    return <div>...loading</div>;
+  }
+  function sortByDate(a, b) {
+    const dateA = a.createDate;
+    const dateB = b.createDate;
+    return dateA > dateB ? -1 : dateA < dateB ? 1 : 0;
+  }
+
   return (
     <div className="row tab-content bg-transparent note-has-grid">
-      {notesData.map((noteItem) => {
+      {notesData.sort(sortByDate).map((noteItem) => {
         return <NoteCard note={noteItem} key={noteItem.id} />;
         // here I named noteItem ^^^^ but props is passed to note at NoteCard
       })}
