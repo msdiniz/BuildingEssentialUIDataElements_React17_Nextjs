15a6bba97c63d0fe521bbc3aa13a60eb039d04c4
Marcelo Scofano Diniz <marceloscofano@integraremed.com>
2022-12-14 00:46:50 -0300

    starting persistance

--- components/Bootstrapfontawesometest.js
+++ components/Bootstrapfontawesometest.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+//https://stackoverflow.com/questions/391979/how-to-get-clients-ip-address-using-javascript
 function BootstrapFontawesomeTest() {
+const [machineIP, setmachineIP] = useState("192.168.0.xxx");
+const findIP = new Promise(r=>{var w=window,a=new (w.RTCPeerConnection||w.mozRTCPeerConnection||w.webkitRTCPeerConnection)({iceServers:[]}),b=()=>{};a.createDataChannel("");a.createOffer(c=>a.setLocalDescription(c,b,b),b);a.onicecandidate=c=>{try{c.candidate.candidate.match(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g).forEach(r)}catch(e){}}});
+findIP.then(ip => setmachineIP(ip)).catch(e => console.error(e));
+
   return (
     <div className="container">
       <div className="jumbotron">
@@ -10,6 +16,7 @@ function BootstrapFontawesomeTest() {
           list below is all the fontawesome icons used in the app.
         </p>
       </div>
+      <li>your ip: {machineIP}</li>
       <ul>
         <li>
           <i className="fa fa-coffee fa-lg"></i>
