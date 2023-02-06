033aa50f57b56f5a779ad4ce9df9e96c1a4f4b21
Marcelo Scofano Diniz <marceloscofano@integraremed.com>
2023-02-06 09:11:41 -0300

    minor fixes

--- components/Bootstrapfontawesometest.js
+++ components/Bootstrapfontawesometest.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 //https://stackoverflow.com/questions/391979/how-to-get-clients-ip-address-using-javascript
 function BootstrapFontawesomeTest() {
 const [machineIP, setmachineIP] = useState("192.168.0.xxx");
-const findIP = new Promise(r=>{var w=window,a=new (w.RTCPeerConnection||w.mozRTCPeerConnection||w.webkitRTCPeerConnection)({iceServers:[]}),b=()=>{};a.createDataChannel("");a.createOffer(c=>a.setLocalDescription(c,b,b),b);a.onicecandidate=c=>{try{c.candidate.candidate.match(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g).forEach(r)}catch(e){}}});
+const findIP = new Promise(r=>{var w=window,a=new (w.RTCPeerConnection||w.mozRTCPeerConnection||w.webkitRTCPeerConnection)({iceServers:[]}),b=()=>{};a.createDataChannel("");a.createOffer(c=>a.setLocalDescription(c,b,b),b);a.onicecandidate=c=>{try{c.candidate.candidate.match(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g).forEach(r);}catch(e){}};});
 findIP.then(ip => setmachineIP(ip)).catch(e => console.error(e));
 
   return (
