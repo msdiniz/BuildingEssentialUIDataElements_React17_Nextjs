9bf9e778e2f2b39205d149ee9b6352e6c46a8a27
Marcelo Scofano Diniz <msdiniz@gmail.com>
2023-02-13 21:32:38 -0300

    fixed eslint errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-// import '/style/styles.css';
+import '../public/style/styles.css';
+import '../public/style/bootstrap.min.css';
+import '../public/style/fontawesome/css/all.css';
+// https://nextjs.org/docs/messages/no-css-tags
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
