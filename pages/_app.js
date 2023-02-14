c7a9c6eb4ebae39c0f86243b43acd45416ea60c2
Marcelo Scofano Diniz <marceloscofano@integraremed.com>
2023-02-14 00:31:41 -0300

    fixed some minor errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import '../public/style/styles.css';
-import '../public/style/bootstrap.min.css';
-import '../public/style/fontawesome/css/all.css';
+import "../public/style/bootstrap.min.css";
+import "../public/style/fontawesome/css/all.css";
+import "../public/style/styles.css";
 // https://nextjs.org/docs/messages/no-css-tags
 
 function MyApp({ Component, pageProps }) {
